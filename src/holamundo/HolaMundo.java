123639af7a6bc41a0d3ddb790de4eee455421f8d
Jose <Jose@DESKTOP-706C4K4>
2020-05-24 10:21:55 +0100

    Segundo commit

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -13,6 +13,7 @@ public class HolaMundo {
     
     public static void main(String args[]){
         System.out.println("Hola mundo");
+        System.out.println("Segundo commit");
     }
     
 }
